aa5857bdc606e4ed212ce3bb434b49f9c6bbe231
Ulises <ulisesp.luna@gmail.com>
2024-01-23 12:58:34 -0600

    modificaciones para que no refresque la página

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ importScripts('js/sw-utils.js');
 
 
 const STATIC_CACHE      = 'static-v2';
-const DYNAMIC_CACHE     = 'dynamic-v1';
+const DYNAMIC_CACHE     = 'dynamic-v2';
 const INMUTABLE_CACHE   = 'inmutable-v1';
 
 
@@ -56,23 +56,25 @@ self.addEventListener('install', e => {
 
 
 self.addEventListener('activate', e => {
-
-
-    const respuesta = caches.keys()
-        .then(keys => {
-            keys.forEach(key => {
-
-                if(key !== STATIC_CACHE && key.includes('static')){
-                    return caches.delete(key);
-                }
-
-            });
+     
+    const respuesta = caches.keys().then( keys => {
+ 
+        keys.forEach( key => {
+ 
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+ 
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+ 
         });
-
-    e.waitUntil(respuesta);
-
-
-
+ 
+    });
+ 
+    e.waitUntil( respuesta );
+ 
 });
 
 
